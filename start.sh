576d79f37f3030d6dd559ca613a69a86b7b19ba2
YvonneGi <giryvonne@gmail.com>
2019-09-19 15:24:31 +0200

    first commit

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY=a492717ad6be358381a84c3ad097772e
+export SECRET_KEY='we code'
+export MAIL_USERNAME=yvogi2780@gmail.com
+export MAIL_PASSWORD=rwanda1994
+python3.6 manage.py server
